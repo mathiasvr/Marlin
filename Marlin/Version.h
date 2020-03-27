2bf30ef5c1f7414aaf22fa9f6fa0f21a04e7b052
Mathias Rasmussen <mathiasvr@gmail.com>
2020-03-27 01:39:05 +0100

    version info

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-//#define SHORT_BUILD_VERSION "2.0.5.3"
+#define SHORT_BUILD_VERSION "2.0.5.3-slim"
 
 /**
  * Verbose version identifier which should contain a reference to the location
@@ -54,7 +54,7 @@
  * has a distinct Github fork— the Source Code URL should just be the main
  * Marlin repository.
  */
-//#define SOURCE_CODE_URL "https://github.com/MarlinFirmware/Marlin"
+#define SOURCE_CODE_URL "https://github.com/mathiasvr/Marlin"
 
 /**
  * Default generic printer UUID.
